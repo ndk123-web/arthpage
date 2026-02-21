a1d31e9296ef8a7e24695d3069f6607837d28291
Navnath Kadam <navnathkadam284@gmail.com>
2026-02-21 12:57:02 +0530

    refactor: remove deprecated dependency and update Vite config
    - Removed "@google/genai" from dependencies in package.json.
    - Updated Vite configuration to include a chunk size warning limit of 1000 KB.
    - Cleaned up import statements and comments in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,31 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-
-// here added your Vite config with the crx plugin 
+// here added your Vite config with the crx plugin
 // crx plugin will read the manifest file and bundle your extension accordingly
 
 // what crx does ?
-  // it tells the vite that build a chrome extension not a regular web app
+// it tells the vite that build a chrome extension not a regular web app
 export default defineConfig({
-  plugins: [react(), crx({ manifest }), tailwindcss(),],
+  plugins: [react(), crx({ manifest }), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 
+  build: {
+    chunkSizeWarningLimit: 1000, // Set the chunk size warning limit to 1000 KB
+    
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks: {
+    //       highlight: ["highlight.js"],
+    //     },
+    //   },
+    // },
+  },
 });
